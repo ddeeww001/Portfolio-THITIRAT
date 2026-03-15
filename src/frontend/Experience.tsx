659f18b69a0123514c7cab37cd5ca4e9e40f9d30
Thitirat Sirisawad <thitirat_sirisawad@cmu.ac.th>
2026-03-15 13:58:52 +0700

    feat: update project entries in Experience component with new details and links

--- src/frontend/Experience.tsx
+++ src/frontend/Experience.tsx
@@ -104,8 +104,98 @@ export const getProject = (id:number,
 }
 
 export const MyProject:ProjectData[]=[
-    //2024//
-    {
+    
+
+{
+    id:8,
+    title:"MY PROJECT DESIGN WEBSITE",
+    date:"NOW",
+    role: "Frontend & UX/UI Designer",
+    details:[],
+    link:[
+        {label:"Project",url:"https://www.figma.com/design/f91hiWcRtLAB5SnPPrtv6U/Untitled?node-id=0-1&t=wfEWnCKOQ9U5G10H-1"},
+        ]
+},
+{
+    id:7,
+    title:"HACKATHON : ETHChaingmai",
+    date:"28 / 01 /2026 - 3 / 02 / 2026",
+    role: "Frontend",
+    details:["My role involved customizing specific parts of the homepage design. Additionally, I implemented the map feature by connecting to an API to fetch and render location"
+    ],
+    link:[
+        {label:"Project",url:"https://devfolio.co/projects/relief-mesh-7406"},
+        {label:"Presentation",url:"https://www.canva.com/design/DAHAEBR-F5o/kU3WI2J8YAblFhI51HkCuA/view?utm_content=DAHAEBR-F5o&utm_campaign=designshare&utm_medium=link&utm_source=viewer"}
+    ]
+},
+{
+    id:6,
+    title:" YOUNG DEV HACKATHON",
+    date:"11 /  01  /  2026 ",
+    role: "STAFF",
+    details:[ "Instructed participants on the web design process, specifically covering user flows, wireframes, and essential design basics."
+       
+    ],
+    link:[
+        {label:"Facebook",url:"https://www.facebook.com/share/p/1BgvpSZCgo/"}
+    ]
+},
+//2026//
+
+{
+    id:5,
+    title:"CAMT open house : WEB3 Clup,Design,Blochchain,Chaing Mai",
+    date:"27 / 8 / 2025",
+    role: "Designer",
+    details:["I was responsible for the end-to-end booklet design, logo creation, and design poster customization"
+       // ,"รับผิดชอบในส่วนของการออกแบบบุคเล็ตตั้งแต่ต้นจนจบ การสร้างโลโก้ และ ปรับแต่งโปสเตอร์"
+    ],
+    link:[
+        {label:"Booklet",url:"https://heyzine.com/flip-book/6c7e35871a.html?ref=web3.camt.cmu.ac.th"},
+        {label:"Website Web3",url:"https://web3.camt.cmu.ac.th/"}
+    ]
+},
+{
+    id:4,
+    title:"Design templates ",
+    date:"4 / 11 / 2025 -  8 / 11 / 2025",
+    role: "Designner",
+    details:["Design templates ligth team and dark team for  COLLEG OF ARTS, MEDIA AND TECHNOLOGY"
+        //, "ออกแบบเทมเพลตสำหรับการทำสไลด์โดยมีทั้งทีมสีอ่อนและทีมสีเข่ม สำหรับสาขา DII วิทยาลัยศิลปะ สื่อ และเทคโนโลยี"
+    ],
+    link:[
+        {label:"— LIGHT TEAM",url:"https://www.canva.com/design/DAG-MThjO7s/QHk_AUP7K2tppS4AOaaVSw/view?utm_content=DAG-MThjO7s&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hb480482753"},
+        {label:"— DARK TEAM",url:"https://www.canva.com/design/DAG-MfTGOD0/8Ecgfe9gtOXGT0X4QVi89Q/view?utm_content=DAG-MfTGOD0&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h0db13d2742"}
+    ]
+},
+{
+    id:3,
+    title:"GROUP PROJECT :  SMART ACCOUNTING AND MANAGEMENT",
+    date:"27 / 8 / 2025",
+    role: "Frontend developer, UX/UI Designer",
+    details:["Designed the user interface for product selection and customized dashboards to track sales and restock activities."
+    ],
+    link:[
+        {label:"SMART ACCOUNTING AND MANAGEMENT",url:"https://www.canva.com/design/DAG-RdyzwBM/DpWwYhyVe11hhewbgSG1Aw/view?utm_content=DAG-RdyzwBM&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h5d0f09fba1"}
+    ]
+    
+},
+{
+    id:2,
+    title:"HACKATHON : Dsign mascot CENTRAL THAM",
+    date:"30 / 10 /2025 - 31 / 10 /2025",
+    role: "Design mascot",
+    details:["Collaborated with a teammate to put our heads together to brainstorm and create a mascot."
+      //,"ทำงานร่วมกับธีมได้ช่วยกันคิดออกแบบมาสคอตพร้อมทั้งช่วยกันออกแบบดีไซน์"
+    ],
+    link:[
+        {label:"Google drive",url:"https://drive.google.com/drive/folders/1yHbN5bEUX7J-nh5HPlfEDBv5ML67_Qqt?usp=sharing"},
+        {label:"CENTRAL THAM hackathon",url:"https://www.centraltham.com/th/newsroom/news-and-updates/150/central-tham-mascot-contest-illustration-impact-with-central-tham?fbclid=IwVERDUANwNt5leHRuA2FlbQIxMQABHvyW9tTacB4Tazik376LqPpoJUTVxjyg2cep4NC0u2eoOM1lKEtpr4APLc_q_aem_jeqlMmr6v_LT0I8hTT1W7g"}
+    ]
+},
+ //2025//
+
+   {
     id:1,
     title:"GRAND SUWAN",
     date:"5 / 11 / 2024",
@@ -121,87 +211,8 @@ export const MyProject:ProjectData[]=[
     ]
 
 },
+//2024//
 
-    //2025//
-    {
-    id:2,
-    title:"HACKATHON : Dsign mascot CENTRAL THAM",
-    date:"30 / 10 /2025 - 31 / 10 /2025",
-    role: "Design mascot",
-    details:["Collaborated with a teammate to put our heads together to brainstorm and create a mascot."
-      //,"ทำงานร่วมกับธีมได้ช่วยกันคิดออกแบบมาสคอตพร้อมทั้งช่วยกันออกแบบดีไซน์"
-    ],
-    link:[
-        {label:"Google drive",url:"https://drive.google.com/drive/folders/1yHbN5bEUX7J-nh5HPlfEDBv5ML67_Qqt?usp=sharing"},
-        {label:"CENTRAL THAM hackathon",url:"https://www.centraltham.com/th/newsroom/news-and-updates/150/central-tham-mascot-contest-illustration-impact-with-central-tham?fbclid=IwVERDUANwNt5leHRuA2FlbQIxMQABHvyW9tTacB4Tazik376LqPpoJUTVxjyg2cep4NC0u2eoOM1lKEtpr4APLc_q_aem_jeqlMmr6v_LT0I8hTT1W7g"}
-    ]
-},
-
-    {
-    id:3,
-    title:"GROUP PROJECT :  SMART ACCOUNTING AND MANAGEMENT",
-    date:"27 / 8 / 2025",
-    role: "Frontend developer, UX/UI Designer",
-    details:["Designed the user interface for product selection and customized dashboards to track sales and restock activities."
-    ],
-    link:[
-        {label:"SMART ACCOUNTING AND MANAGEMENT",url:"https://www.canva.com/design/DAG-RdyzwBM/DpWwYhyVe11hhewbgSG1Aw/view?utm_content=DAG-RdyzwBM&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h5d0f09fba1"}
-    ]
-    
-},
-{
-    id:4,
-    title:"Design templates ",
-    date:"4 / 11 / 2025 -  8 / 11 / 2025",
-    role: "Designner",
-    details:["Design templates ligth team and dark team for  COLLEG OF ARTS, MEDIA AND TECHNOLOGY"
-        //, "ออกแบบเทมเพลตสำหรับการทำสไลด์โดยมีทั้งทีมสีอ่อนและทีมสีเข่ม สำหรับสาขา DII วิทยาลัยศิลปะ สื่อ และเทคโนโลยี"
-    ],
-    link:[
-        {label:"— LIGHT TEAM",url:"https://www.canva.com/design/DAG-MThjO7s/QHk_AUP7K2tppS4AOaaVSw/view?utm_content=DAG-MThjO7s&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=hb480482753"},
-        {label:"— DARK TEAM",url:"https://www.canva.com/design/DAG-MfTGOD0/8Ecgfe9gtOXGT0X4QVi89Q/view?utm_content=DAG-MfTGOD0&utm_campaign=designshare&utm_medium=link2&utm_source=uniquelinks&utlId=h0db13d2742"}
-    ]
-},
-
-{
-    id:5,
-    title:"CAMT open house : WEB3 Clup,Design,Blochchain,Chaing Mai",
-    date:"27 / 8 / 2025",
-    role: "Designer",
-    details:["I was responsible for the end-to-end booklet design, logo creation, and design poster customization"
-       // ,"รับผิดชอบในส่วนของการออกแบบบุคเล็ตตั้งแต่ต้นจนจบ การสร้างโลโก้ และ ปรับแต่งโปสเตอร์"
-    ],
-    link:[
-        {label:"Booklet",url:"https://heyzine.com/flip-book/6c7e35871a.html?ref=web3.camt.cmu.ac.th"},
-        {label:"Website Web3",url:"https://web3.camt.cmu.ac.th/"}
-    ]
-},
-
-//2026//
-{
-    id:6,
-    title:" YOUNG DEV HACKATHON",
-    date:"11 /  01  /  2026 ",
-    role: "STAFF",
-    details:[ "Instructed participants on the web design process, specifically covering user flows, wireframes, and essential design basics."
-       
-    ],
-    link:[
-        {label:"Facebook",url:"https://www.facebook.com/share/p/1BgvpSZCgo/"}
-    ]
-},
-{
-    id:7,
-    title:"HACKATHON : ETHChaingmai",
-    date:"28 / 01 /2026 - 3 / 02 / 2026",
-    role: "Frontend",
-    details:["My role involved customizing specific parts of the homepage design. Additionally, I implemented the map feature by connecting to an API to fetch and render location"
-    ],
-    link:[
-        {label:"Project",url:"https://devfolio.co/projects/relief-mesh-7406"},
-        {label:"Presentation",url:"https://www.canva.com/design/DAHAEBR-F5o/kU3WI2J8YAblFhI51HkCuA/view?utm_content=DAHAEBR-F5o&utm_campaign=designshare&utm_medium=link&utm_source=viewer"}
-    ]
-},
 ];
 
 
